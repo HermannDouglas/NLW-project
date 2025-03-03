726765b4239b2cd626760102030811b94f2fc6ef
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-03-03 14:25:48 -0500

    Feat: Adicionar repository e conexão com BD
    - Adiciona repository de Eventos
    - Implementa conexão com Banco de Dados
    - Implementa teste de inserção e seleção de dados da tabela Eventos

--- src/model/entities/eventos.py
+++ src/model/entities/eventos.py
@@ -1,9 +1,9 @@
 from src.model.configs.base import Base
-from slqalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer
 
 
 class Eventos(Base):
     __tablename__ = "Eventos"
 
-    id = Column(Integer, primary_key=True, autoinvrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String, nullable=False)
