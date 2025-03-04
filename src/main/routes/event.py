aa4a0db3e59ad98cd59b9cc16f9c94bb5f1eb0cf
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-03-04 11:43:15 -0500

    Feat: Implementar validators
    - Adiciona validators para o json de Eventos e Inscritos

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.http_types.http_response import HttpResponse
 from src.http_types.http_request import HttpRequest
+from src.validators.events_creator_validator import events_creator_validator
 
 
 event_route_bp = Blueprint("event_route", __name__)
@@ -8,7 +9,8 @@ event_route_bp = Blueprint("event_route", __name__)
 
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
+    events_creator_validator(request)
     http_request = HttpRequest(body=request.json)
-    
+
     http_response = HttpResponse(body={"Hello": "world"}, status_code=201)
     return jsonify(http_response.body), http_response.status_code
