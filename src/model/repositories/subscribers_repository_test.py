24545b9c0c48913e0fd6c79d9ce7531186218209
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-03-05 11:32:05 -0500

    Feat: Adiconar novos métodos
    - Adiconar busca de inscritos pelo link de inscrição
    - Adiciona ranking dos inscritos por link de inscrição

--- src/model/repositories/subscribers_repository_test.py
+++ src/model/repositories/subscribers_repository_test.py
@@ -12,11 +12,24 @@ def test_insert():
     subs_repo = SubscribersRepository()
     subs_repo.insert(subscriber_info)
 
+
 @pytest.mark.skip("Select in DB")
 def test_select_subscriber():
     email = "email@email.com"
     evento_id = 2
 
     subs_repo = SubscribersRepository()
-    resp = subs_repo.select_subscriber(email,evento_id)
+    resp = subs_repo.select_subscriber(email, evento_id)
     print(resp.nome)
+
+
+@pytest.mark.skip("Select in DB")
+def test_ranking():
+    evento_id = 3
+    subs_repo = SubscribersRepository()
+    resp = subs_repo.get_ranking(evento_id)
+
+    print()
+
+    for elem in resp:
+        print(f"Link: {elem.link}, total de inscritos: {elem.total}")
